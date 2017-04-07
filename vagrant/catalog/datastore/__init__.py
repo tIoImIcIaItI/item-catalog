9163a2d86f8d4ba1ec49a3ae8c20294df85ca5f2
tIoImIcIaItI <djkinggithub@outlook.com>
2017-04-07 09:22:37 -0600

    Put database connection info into excluded file
    * Create database from config dictionary.

--- vagrant/catalog/datastore/__init__.py
+++ vagrant/catalog/datastore/__init__.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from datastore import config
+
 Base = declarative_base()
-engine = create_engine('sqlite:///itemcatalog.db')
+engine = create_engine(URL(**config.DATABASE))
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
